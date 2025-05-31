b050ca82705c785a65e5633a0328669c86a283e0
crymang <uamind@foxmail.com>
2025-05-31 19:50:13 +0800

    Merge remote changes and resolve conflicts

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -12,7 +12,7 @@ export default defineGkdApp({
         {
           fastQuery: true,
           excludeActivityIds: '.main.MainActivity',
-          matches: '[vid="f6t"][desc^="未点赞"]',
+          matches: '[vid="f6t"][desc^="未点赞"]:nth-match(1)',
           actionDelay: 300,
           snapshotUrls: [], // 这里可以添加相关截图的URL
         },
