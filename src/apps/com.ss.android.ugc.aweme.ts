0e2f4e4b7b5b3d776733eed6651cb11c636e9901
crymang <uamind@foxmail.com>
2025-06-01 20:19:28 +0800

    Merge remote changes and resolve conflicts

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -4,23 +4,6 @@ export default defineGkdApp({
   id: 'com.ss.android.ugc.aweme',
   name: '抖音',
   groups: [
-    // {
-    //   key: 0,
-    //   name: '点赞',
-    //   desc: '自动点赞操作', // 添加描述
-    //   rules: [
-    //     {
-    //       fastQuery: true,
-    //       excludeActivityIds: '.main.MainActivity',
-    //       matches: '[id="com.ss.android.ugc.aweme:id/f59"]',
-
-    //       // matches: '[id="com.ss.android.ugc.aweme:id/f6t"][desc^="未点赞"]',
-    //       // matches: '[vid="wqh"] > [vid="f6e"] > [vid="f6t"][desc^="未点赞"] > [vid="f58"] > [vid="f5t"] > @ImageView',
-    //       actionDelay: 800,
-    //       snapshotUrls: [], // 这里可以添加相关截图的URL
-    //     },
-    //   ],
-    // },
     {
       key: 0,
       name: '点赞',
