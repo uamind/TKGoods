d5f11eb9c62d758d60f03afb9fd3ee07140be5b2
crymang <uamind@foxmail.com>
2025-06-01 20:12:13 +0800

    Merge remote changes and resolve conflicts

--- src/apps/com.ss.android.ugc.aweme.ts
+++ src/apps/com.ss.android.ugc.aweme.ts
@@ -4,20 +4,38 @@ export default defineGkdApp({
   id: 'com.ss.android.ugc.aweme',
   name: '抖音',
   groups: [
-    {
+    // {
+    //   key: 0,
+    //   name: '点赞',
+    //   desc: '自动点赞操作', // 添加描述
+    //   rules: [
+    //     {
+    //       fastQuery: true,
+    //       excludeActivityIds: '.main.MainActivity',
+    //       matches: '[id="com.ss.android.ugc.aweme:id/f59"]',
+
+    //       // matches: '[id="com.ss.android.ugc.aweme:id/f6t"][desc^="未点赞"]',
+    //       // matches: '[vid="wqh"] > [vid="f6e"] > [vid="f6t"][desc^="未点赞"] > [vid="f58"] > [vid="f5t"] > @ImageView',
+    //       actionDelay: 800,
+    //       snapshotUrls: [], // 这里可以添加相关截图的URL
+    //     },
+    //   ],
+    // },
+        {
       key: 0,
-      name: '点赞',
-      desc: '自动点赞操作', // 添加描述
+      name: '收藏',
+      desc: '自动收藏操作',
       rules: [
         {
           fastQuery: true,
-          excludeActivityIds: '.main.MainActivity',
-          matches: '[id="com.ss.android.ugc.aweme:id/f59"]',
-
-          // matches: '[id="com.ss.android.ugc.aweme:id/f6t"][desc^="未点赞"]',
-          // matches: '[vid="wqh"] > [vid="f6e"] > [vid="f6t"][desc^="未点赞"] > [vid="f58"] > [vid="f5t"] > @ImageView',
-          actionDelay: 800,
-          snapshotUrls: [], // 这里可以添加相关截图的URL
+          activityIds: '.main.MainActivity',
+          matches: '[vid="wqh"] > [vid="d3d"] > [vid="d4a"][desc^="未选中"]',
+          actionDelay: 500,
+          snapshotUrls: [],
+          position: {
+            x: 1001,
+            y: 1366
+          }
         },
       ],
     },
@@ -30,7 +48,7 @@ export default defineGkdApp({
           fastQuery: true,
           activityIds: '.main.MainActivity',
           matches: '[vid="wqh"] > [vid="d3d"] > [vid="d4a"][desc^="未选中"]',
-          actionDelay: 500,
+          actionDelay: 800,
           snapshotUrls: [],
         },
       ],
